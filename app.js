3b7f96eb62c80b46938ae440cd1a11ac932c2f18
Namb2306630 <namhuynh0914923314@gmail.com>
2026-05-23 23:24:50 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express'); //Import thư viện: express để tạo server
 const cors = require('cors'); //Import thư viện: cors để cho phép frontend khác port gọi backend
 
-const contactRouter = require('./app/routes/contact.route'); //Import router contact
+const contactRouter = require('./app/routes/contact.route'); 
+
+const ApiError = require('./app/api-error'); 
 
 const app = express(); //app chính là server
 
@@ -13,5 +15,16 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/contacts', contactRouter); //Khi có request đến /api/contacts sẽ được xử lý bởi contactRouter
+
+// Handle 404 route not found
+app.use((req, res, next) => {
+  return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
 
 module.exports = app;
